cea4d9da4eca63c52c8edae409c892637782212e
Alekdandr Velmozhko <wewufamily@gmail.com>
2023-02-28 20:33:38 +0200

    Completed the tasks -sort strings -pick -omit

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,12 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
+  const result = [...arr];
+  result.sort((a, b) => {
+    return a.localeCompare(b, ["ru", "en"], {
+      caseFirst: "upper",
+    });
+  });
+  return param === "asc" ? result : result.reverse();
 }
