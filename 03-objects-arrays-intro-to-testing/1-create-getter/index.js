a0f341f0ced24f44961f94063477bafe2ecf1d05
Alekdandr Velmozhko <wewufamily@gmail.com>
2023-03-05 13:57:23 +0200

    moved path definition to parent function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,8 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArray = path.split(".");
   return function (obj) {
-    const pathArray = path.split(".");
     return pathArray.reduce((acc, currentValue) => acc?.[currentValue], obj);
   };
 }
