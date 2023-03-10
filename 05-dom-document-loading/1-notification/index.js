da927ea439e5776e5e778a009a313ec09042b1db
Alekdandr Velmozhko <wewufamily@gmail.com>
2023-03-10 15:48:55 +0200

    completed component NotificationMessage

---
+++ 05-dom-document-loading/1-notification/index.js
@@ -0,0 +1,53 @@
+export default class NotificationMessage {
+  constructor(message = "", { duration = 0, type = "" } = {}) {
+    this.text = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.getNotificationMessage();
+    this.element = wrapper.firstElementChild;
+  }
+
+  getNotificationMessage() {
+    return `
+    <div class="notification ${this.type}" style="--value:
+    ${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.text}
+      </div>
+    </div>
+  </div>
+    `;
+  }
+
+  show(parentElement = document.body) {
+    let notification = document.querySelector(".notification");
+    if (notification) {
+      notification.remove();
+    }
+
+    parentElement.append(this.element);
+    setTimeout(() => {
+      this.destroy();
+      console.log(this.element);
+    }, this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
+}
